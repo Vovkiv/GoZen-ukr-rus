b331ebbb912d2d0ec47a10bc43e2f4f278e825ee
voylin <0voylin0@gmail.com>
2024-02-13 15:13:11 +0900

    re-design

--- generate_mo_files.sh
+++ generate_mo_files.sh
@@ -2,14 +2,14 @@
 
 echo "Generating *.mo files"
 
+pushd $(dirname "$0")/po_files
 for po_file in *.po; do
   if [ -e "$po_file" ]; then
     # Generate .mo file
-    msgfmt -o "${po_file%.po}.mo" "$po_file"
+    msgfmt -o "../../${po_file%.po}.mo" "$po_file"
     echo "Generated: ${po_file%.po}.mo"
   fi
 done
-
-mv *.mo ../
+popd
 
 echo "Generating localization files complete!"
